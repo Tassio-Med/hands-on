4f44626d326377a67b8781099ca379f25c8b2324
Tassio-Med <tassiohfmed@gmail.como>
2023-07-06 14:52:10 -0300

    feat: criada aplicação

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('Olá, mundo!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3020;
+app.use((0, helmet_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send('<h1>Olá, seja bem-vindo ao Projeto Hands On!<h1/>');
+});
+app.listen(PORT, () => console.log(`Servidor execeutando na porta ${PORT}`));
